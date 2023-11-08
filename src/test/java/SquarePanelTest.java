9cec66dc4f5e2e0e09dbcd5b5ef71b98701b7968
CHAN, Chun Hin <chchanec@ust.hk>
2023-11-08 16:42:50 +0800

    Include a new unit test: SquarePanelTest.java

---
+++ src/test/java/SquarePanelTest.java
@@ -0,0 +1,13 @@
+import java.awt.*;
+
+import static org.junit.Assert.*;
+
+public class SquarePanelTest {
+
+    void SquarePanelTest1()
+    {
+        SquarePanel p1 = new SquarePanel(Color.white) ;
+
+    }
+
+}
