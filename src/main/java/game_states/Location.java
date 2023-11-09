f9c3490f64b199d5db8d4d0b83fbdc52a67f10b0
CHAN, Chun Hin <chchanec@ust.hk>
2023-11-09 21:58:00 +0800

    JavaDoc documentation for opposite_node() has just completed.
    All JavaDoc documentation for function A are done now.

--- src/main/java/game_states/Location.java
+++ src/main/java/game_states/Location.java
@@ -32,25 +32,80 @@ public record Location(int row, int col) {
         return "[ "+row+" , "+col+" ]";
     }
 
+    /**
+     * opposite_node() is a member function for Location class.
+     * It returns the opposite node of the current node, and it should be exactly the adjacent node of the current node;
+     * or null.
+     *
+     * @param l: The parent node of the current node that call this member function
+     * @return The opposite node of the current node, and it should be exactly the adjacent node of the current node; or null.
+     */
     public Location opposite_node(Location l) {
+        /**
+         * Situation A: Both parent node and the current node are in the column
+         */
+
+        /**
+         * Case A1: The current node is below the parent node
+         * return the node that is directly below and adjacent to the current node
+         *
+         * Picture demonstration:
+         *
+         * [parent node]
+         * [current node]
+         * [return node]
+         */
         if (this.row > l.row)
         {
             return new Location(this.row + 1, this.col) ;
         }
+        /**
+         * Case A2: The current node is above the parent node
+         * return the node that is directly above and adjacent to the current node
+         *
+         * Picture demonstration:
+         *
+         * [return node]
+         * [current node]
+         * [parent node]
+         */
         if (this.row < l.row)
         {
             return new Location(this.row - 1, this.col) ;
         }
 
+        /**
+         * Situation B: Both parent node and the current node are in the row
+         */
+
+        /**
+         * Case B1: The current node is on the right side of the parent node
+         * return the node that is directly on the right of and adjacent to the current node
+         *
+         * Picture demonstration:
+         *
+         * [parent node] [current node] [return node]
+         */
         if (this.col > l.col)
         {
             return new Location(this.row, this.col + 1) ;
         }
+        /**
+         * Case B2: The current node is on the left side of the parent node
+         * return the node that is directly on the left of and adjacent to the current node
+         *
+         * Picture demonstration:
+         *
+         * [left node] [current node] [parent node]
+         */
         if (this.col < l.col)
         {
             return new Location(this.row, this.col - 1) ;
         }
 
+        /**
+         * If none of the above if statements are satisfied, it would return null.
+         */
         return null ;
     }
 }
