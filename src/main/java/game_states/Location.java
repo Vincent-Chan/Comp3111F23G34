92522d8e2bdaf495bb1b4fac757754abf68bf347
edwardxiaoyicong <yxiaoaz@connect.ust.hk>
2023-11-18 16:49:46 +0800

    Test case added

--- src/main/java/game_states/Location.java
+++ src/main/java/game_states/Location.java
@@ -3,31 +3,109 @@ package game_states;
 import java.util.Objects;
 
 /**
- * Data structure to represent a location on the map
- * @param row the row of the location (0-indexed, increases from up to down)
- * @param col the column of the location (0-indexed, increases from left to right)
- * */
+ * The class Location involves 2 data members, which are row and col.
+ *
+ * @param row: Stores the row index of the location (i.e. node)
+ * @param col: Stores the column index of the location (i.e. node)
+ *
+ *
+ *
+ * This class also involves 2 member functions.
+ *
+ * The function toString() will convert the location into a String expression of the location.
+ *
+ * The function opposite_node() accepts Location instance (the parent node of the current node) as input parameter,
+ * then return a node that is adjacent to the current node (i.e. the node that call this member function) and opposite to the parent node.
+ */
+
 public record Location(int row, int col) {
 
+    /**
+     * toString() is a member function for Location class.
+     * It returns the String expression of the location.
+     *
+     * @return: A string that store the string expression of the current location in the format [ row , col ]
+     */
+    @Override
+    public String toString()
+    {
+        return "[ "+row+" , "+col+" ]";
+    }
+
+    /**
+     * opposite_node() is a member function for Location class.
+     * It returns the opposite node of the current node, and it should be exactly the adjacent node of the current node;
+     * or null.
+     *
+     * @param l: The parent node of the current node that call this member function
+     * @return The opposite node of the current node, and it should be exactly the adjacent node of the current node; or null.
+     */
     public Location opposite_node(Location l) {
+        /**
+         * Situation A: Both parent node and the current node are in the column
+         */
+
+        /**
+         * Case A1: The current node is below the parent node
+         * return the node that is directly below and adjacent to the current node
+         *
+         * Picture demonstration:
+         *
+         * [parent node]
+         * [current node]
+         * [return node]
+         */
         if (this.row > l.row)
         {
             return new Location(this.row + 1, this.col) ;
         }
+        /**
+         * Case A2: The current node is above the parent node
+         * return the node that is directly above and adjacent to the current node
+         *
+         * Picture demonstration:
+         *
+         * [return node]
+         * [current node]
+         * [parent node]
+         */
         if (this.row < l.row)
         {
             return new Location(this.row - 1, this.col) ;
         }
 
+        /**
+         * Situation B: Both parent node and the current node are in the row
+         */
+
+        /**
+         * Case B1: The current node is on the right side of the parent node
+         * return the node that is directly on the right of and adjacent to the current node
+         *
+         * Picture demonstration:
+         *
+         * [parent node] [current node] [return node]
+         */
         if (this.col > l.col)
         {
             return new Location(this.row, this.col + 1) ;
         }
+        /**
+         * Case B2: The current node is on the left side of the parent node
+         * return the node that is directly on the left of and adjacent to the current node
+         *
+         * Picture demonstration:
+         *
+         * [left node] [current node] [parent node]
+         */
         if (this.col < l.col)
         {
             return new Location(this.row, this.col - 1) ;
         }
 
+        /**
+         * If none of the above if statements are satisfied, it would return null.
+         */
         return null ;
     }
 }
