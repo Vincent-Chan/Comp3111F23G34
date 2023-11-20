b6a3a6eaa776add18c6314bfc99065ebea26557c
edwardxiaoyicong <112631658+yxiaoaz@users.noreply.github.com>
2023-11-20 16:16:44 +0800

    Delete src/main/java/game_states directory

--- src/main/java/game_states/GameState.java
+++
@@ -1,6 +0,0 @@
-package game_states;
-
-/**The fixed representation of three possible game ending conditions*/
-public enum GameState {
-    JERRY_WIN, TOM_WIN, CONTINUE;
-}
--- src/main/java/game_states/MoveCode.java
+++
@@ -1,6 +0,0 @@
-package game_states;
-
-/**Fixed representation of a movement direction*/
-public enum MoveCode {
-    UP, RIGHT, LEFT, DOWN;
-}
