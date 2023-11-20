6a9687946095b2c78869f263e8d4ad9270ab919f
edwardxiaoyicong <112631658+yxiaoaz@users.noreply.github.com>
2023-11-20 16:23:20 +0800

    Create characterID.java

---
+++ src/main/java/game_entities/characterID.java
@@ -0,0 +1,10 @@
+package game_entities;
+
+/**
+ * {@value #TOM_ID} the character ID of Tom (computer)
+ * {@value #JERRY_ID} the character ID of Jerry (player)
+ */
+public class characterID {
+    public static final int TOM_ID = 0;
+    public static final int JERRY_ID = 1;
+}
